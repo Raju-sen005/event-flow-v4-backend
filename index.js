7eb6e3331e2d995799543787df2fb27e4664df36
Raju Sen <rajusen005@gmail.com>
2026-02-02 17:32:26 +0530

    Backend setup

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import sequelize from "./config/db.js"; // 🔥 YAHI SAHI IMPORT HAI
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json("Hello World !");
+});
+
+// ✅ DB CONNECT & SYNC
+sequelize
+  .sync({ alter: true })
+  .then(() => {
+    console.log("Database connected & synced");
+  })
+  .catch((err) => {
+    console.error("DB connection error:", err);
+  });
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
+});
